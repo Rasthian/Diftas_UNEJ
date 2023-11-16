241092e32e88a8abb932ba17fd8e110a3e269199
haris ardian <harisfirman387@gmail.com>
2023-11-16 11:03:49 +0700

    update login index

--- controller/main.php
+++ controller/main.php
@@ -6,7 +6,7 @@ require_once 'config/config.php';
 $url = $_GET['url'] ?? '/Diftas_UNEJ/';
 switch ($url) {
     case 'login':
-        
+    LoginController::index();
     default:
     DiscussController::index();
 }
