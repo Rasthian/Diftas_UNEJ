9517d9e0c93032038243ec05405570ba4d4ce6ca
Rasthian <81855875+Rasthian@users.noreply.github.com>
2023-11-20 11:47:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,9 @@
 	require_once('config/conn.php');
 	require_once('controller/diskusi.php');
     require_once('controller/login.php');
-
-
-
+	$action = isset($_GET['action']) ? $_GET['action'] : 'index';
 	$diskusiController = new DiskusiController();
     $loginController = new LoginController();
-
-
 	switch ($action) {
         case 'login':
             $loginController->login();  
