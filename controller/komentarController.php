dca981ab01abdf8387257ccca36852e95fcc7e70
haris ardian <harisfirman387@gmail.com>
2023-12-02 10:35:32 +0700

    feat controller (gadipakai)

---
+++ controller/komentarController.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'config/conn.php';
+require_once 'function.php';
+require_once 'model/komentarModels.php';
+
+class diskusiKomentar {
+    public static function add ($idDiskusi) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $isi = $_POST ['isi_komentar'];
+            $err = '' ;
+            $result = ModelKomentar::addComment($isi,$idDiskusi);
+            if ($result) {
+                header("Location: ?action=diskusi&id=$idDiskusi");
+                exit();
+            } else {
+                // Tampilkan pesan error
+                $err = "Gagal menambahkan diskusi. Silakan coba lagi.";
+            }
+        }
+        include 'view/home/subview/show.php';
+    }
+}
+?>
